1f5dccd6ae09797140d31f804805c44062197858
Ubirajara Campos <birapcampos@gmail.com>
2018-12-04 20:44:20 -0200

    Inclusao de segurança com jwt e Spring sevurity

---
+++ src/main/java/br/com/cursoapirest/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package br.com.cursoapirest.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.cursoapirest.entities.Funcionario;
+import br.com.cursoapirest.security.JwtUserFactory;
+import br.com.cursoapirest.services.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
